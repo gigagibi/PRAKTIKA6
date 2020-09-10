f588108a57219f70034c9e4e8fc592e76c1b3142
gigagibi <70891118+gigagibi@users.noreply.github.com>
2020-09-10 13:12:53 +0300

    Update Main.java

--- praktika6 zadanie2/src/com/company/Main.java
+++ praktika6 zadanie2/src/com/company/Main.java
@@ -4,5 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+	MovableRectangle mrect = new MovableRectangle(1, 1, 2, 2, 1, 1);
+	if(mrect.checkSpeeds())
+	{
+		System.out.print("OK");
+	}
+	else
+	{
+		System.out.print("Not OK");
+	}
     }
 }
