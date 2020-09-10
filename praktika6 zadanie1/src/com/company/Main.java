021e2b55ff988c2056fd728fe28bc1173f5a9686
gigagibi <70891118+gigagibi@users.noreply.github.com>
2020-09-10 13:04:47 +0300

    Update Main.java

--- praktika6 zadanie1/src/com/company/Main.java
+++ praktika6 zadanie1/src/com/company/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+	Square sq = new Square();
+	Circle cr = new Circle();
+	cr.setRadius(3);
+	System.out.print(cr.getArea());
     }
 }
